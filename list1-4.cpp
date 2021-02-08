517078653b6d7578d879ec79daf40e0addda4c2a
arataka777 <78463763+arataka777@users.noreply.github.com>
2021-02-08 15:34:48 +0900

    list1-4

---
+++ list1-4.cpp
@@ -0,0 +1,48 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+class Accounting
+{
+    string full_name;
+    long crnt_asset;
+
+    public:
+    Accounting(string name, long amnt);
+
+    string name(){return full_name;}
+    long asset(){return crnt_asset;}
+
+    void earn(long amnt);
+    void spend(long amnt);
+
+};
+
+Accounting::Accounting(string name, long amnt)
+{
+    full_name = name;
+    crnt_asset = amnt;
+}
+
+void Accounting::earn(long amnt)
+{
+    crnt_asset += amnt;
+}
+
+void Accounting::spend(long amnt)
+{
+    crnt_asset -= amnt;
+}
+
+it main()
+{
+    Accounting nakano("中野隼人", 1000);
+    Accounting("山田宏文", 200);
+
+    nakano.spend(200);
+    yamada.earn(100);
+
+    cout << "中野：\"" << nakano.name() << "\" " << nakano.asset() << "円\n"; 
+    cout << "山田：\"" << yamada.name() << "\" " << yamada.asset() << "円\n"; 
+}
